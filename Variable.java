413e58e267b4693649087e7f88a669aeafb571ae
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-03-19 20:13:22 +0530

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,23 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a=10;
+        char alp='a';
+        String name="coder";
+        double num=12.34;
+        long ln=123456789l;
+        boolean tr=true;
+        float fl=12.5f;
+        short sh=123;
+        byte by=12;
+        System.out.println("integer : "+a);
+        System.out.println("character : "+alp);
+        System.out.println("string : "+name);
+        System.out.println("double : "+num);
+        System.out.println("long : "+ln);
+        System.out.println("boolean : "+tr);
+        System.out.println("float : "+fl);
+        System.out.println("short : "+sh);
+        System.out.println("byte : "+by);
+
+    }
+}
